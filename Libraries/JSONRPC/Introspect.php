9be42b742c20af5f84c5bc64e8719d746ddebc3d
Arnaud Lafon <arnaud.lafon@gmail.com>
2012-01-31 22:58:37 +0100

    Added a method to list all available namespace.method (for autogeneration purpose)

--- Libraries/JSONRPC/Introspect.php
+++ Libraries/JSONRPC/Introspect.php
@@ -2,11 +2,17 @@
 
 namespace MMC\XBMCBundle\API\XBMC\Libraries\JSONRPC;
 
-use MMC\XBMCBundle\API\XBMC\Libraries\APIMethod as APIMethod;
+use \MMC\XBMCBundle\API\XBMC\Libraries\APIMethod as APIMethod;
+use \MMC\XBMCBundle\API\XBMC\Server as XBMCServer;
 
 class Introspect extends APIMethod
 {
 
+    public function getAPIMethodsList()
+    {
+        $introspectArray = $this->call( array(), XBMCServer::RETURN_TYPE_ARRAY );
+        return array_keys( $introspectArray['result']['methods'] );
+    }
 
 }
 
