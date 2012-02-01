82678efe3ce95e51995eeb623e70068f5f6a9a19
Arnaud Lafon <arnaud.lafon@gmail.com>
2012-02-01 13:16:18 +0100

    Enhanced API method & namespace generator

--- Libraries/Generator.php
+++ Libraries/Generator.php
@@ -2,32 +2,58 @@
 
 namespace MMC\XBMCBundle\API\XBMC\Libraries;
 
+/**
+ * This class helps us by creating the directory structure and the classes
+ * need to have a great auto-completion feature with the XBMC API
+ *
+ * Usage :
+ * $generator = new Generator( 'AudioLibrary', array( 'Clean', '...' ) );
+ * $generator->generateAndWriteToDisk();
+ *
+ * For the moment, these classes are written in
+ * app/cache/dev/mmc/XBMC/Libraries
+ *
+ */
 class Generator
 {
     private $APINamespace;
-    private $APIMethod;
+    private $APIMethods;
 
     const RETURN_STATUS_CREATED = 0;
     const RETURN_STATUS_EXISTS = 1;
     const RETURN_STATUS_ERROR = 2;
 
-    public function __construct( $APINamespace, $APIMethod )
+    /**
+     *
+     * @param string $APINamespace
+     * @param array $APIMethods
+     */
+    public function __construct( $APINamespace, $APIMethods, $baseLocation = null )
     {
-        $this->APIMethod = $APIMethod;
+        $this->APIMethods = $APIMethods;
         $this->APINamespace = $APINamespace;
+
+        // @todo make it more sexy...
+        $this->baseLocation = $baseLocation == null ? realpath( __DIR__ . "/../../../../../../app/cache/dev" ) . "/mmc/XBMC/Libraries" : $baseLocation;
     }
 
-    public function getCode()
+    /**
+     * Generates the code for a APIMethod class
+     *
+     * @param string $method
+     * @return string
+     */
+    private function getAPIMethodCode( $method )
     {
         $classCode = <<<EOF
 <?php
 
-namespace MMC\XBMCBundle\API\XBMC\Libraries\\$this->APINamespace;
+namespace MMC\\XBMCBundle\\API\\XBMC\\Libraries\\$this->APINamespace;
 
-use \MMC\XBMCBundle\API\XBMC\Libraries\APIMethod as APIMethod;
+use \\MMC\\XBMCBundle\\API\\XBMC\\Libraries\\APIMethod as APIMethod;
 
 /**
- * A simple definition for $this->APINamespace.$this->APIMethod
+ * A simple definition for $this->APINamespace.$method
  * You can add custom methods in this class (see Introspect.getAPIMethodsList()
  * for an example)
  *
@@ -35,7 +61,45 @@ use \MMC\XBMCBundle\API\XBMC\Libraries\APIMethod as APIMethod;
  * @see \MMC\XBMCBundle\API\XBMC\Libraries\Generator
  *
  */
-class $this->APIMethod extends APIMethod
+class $method extends APIMethod
+{
+}
+EOF;
+        return $classCode;
+    }
+
+    /**
+     * Generates the code for a APINamespace class
+     *
+     * @return string
+     */
+    private function getAPINamespaceCode()
+    {
+        $classCode = <<<EOF
+<?php
+
+namespace MMC\\XBMCBundle\\API\\XBMC\\Libraries;
+
+/**
+ * A simple definition for $this->APINamespace
+ *
+ * Note: this class was auto-generated using JSONRPC.Introspect results
+ * @see \MMC\XBMCBundle\API\XBMC\Libraries\Generator
+ *
+EOF;
+        foreach( $this->APIMethods as $method )
+        {
+            $classCode .= <<<EOF
+
+ * @property $this->APINamespace\\$method    \$$method
+EOF;
+        }
+
+        $classCode .= <<<EOF
+
+ *
+ */
+class $this->APINamespace extends APINamespace
 {
 }
 EOF;
@@ -44,37 +108,61 @@ EOF;
 
     private function getLocation()
     {
-        return __DIR__ . DIRECTORY_SEPARATOR . $this->APINamespace;
+
+        return $this->baseLocation;
     }
 
-    private function getFileName()
+    private function getMethodFileName( $method )
     {
-        return "{$this->APIMethod}.php";
+        return "{$method}.php";
     }
 
+    private function getNamespaceFileName()
+    {
+        return "{$this->APINamespace}.php";
+    }
+
+    /**
+     * Generates both APIMethod classes and the main APINamespace class
+     */
     public function generateAndWriteToDisk()
     {
-        $location = $this->getLocation();
-        $filename = $this->getFileName();
-        $filepath = $location . DIRECTORY_SEPARATOR . $filename;
-
-        if(file_exists($filepath))
+        // API Methods for this namespace
+        foreach( $this->APIMethods as $method )
         {
-            return self::RETURN_STATUS_EXISTS;
+            $methodLocation = $this->getLocation() . DIRECTORY_SEPARATOR . $this->APINamespace;
+            $methodFilename = $this->getMethodFileName( $method );
+            $filepath = $methodLocation . DIRECTORY_SEPARATOR . $methodFilename;
+
+            if( !file_exists( $filepath ))
+            {
+                if( !is_dir( $methodLocation ))
+                {
+                    mkdir( $methodLocation, 0777, true );;
+                }
+
+                if( $fh = fopen( $filepath , 'w+' ))
+                {
+                    fwrite( $fh, $this->getAPIMethodCode( $method ) );
+                    fclose( $fh );
+                }
+            }
         }
 
-        if( !is_dir($location))
+        // API Namespace
+        $namespaceLocation = $this->getLocation();
+        $namespaceFilename = $this->getNamespaceFileName();
+        $filepath = $namespaceLocation . DIRECTORY_SEPARATOR . $namespaceFilename;
+
+        if( !file_exists( $filepath ))
         {
-            mkpath( $location );
+            if( $fh = fopen( $filepath , 'w+' ))
+            {
+                fwrite( $fh, $this->getAPINamespaceCode() );
+                fclose( $fh );
+            }
         }
 
-        if( $fh = fopen( $filepath , 'r+' ) )
-        {
-            $written = fwrite( $fh, $this->getCode() ) !== false;
-            fclose( $fh );
-            return $written ? self::RETURN_STATUS_CREATED : self::RETURN_STATUS_ERROR;
-        }
-        return self::RETURN_STATUS_ERROR;
 
 
     }
