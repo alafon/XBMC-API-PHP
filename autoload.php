087d4d25361f0fe2f6c5b8ce84649b3abad19785
Arnaud Lafon <arnaud.lafon@gmail.com>
2012-02-02 11:05:40 +0100

    Removed test echo

--- autoload.php
+++ autoload.php
@@ -2,7 +2,6 @@
 
 function __autoload( $classname )
 {
-    echo $classname;
     $classnameExplode = explode( "\\", $classname );
     $location = implode( "/", array_slice( $classnameExplode, 1) );
     include "{$location}.php";
